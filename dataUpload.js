a95340a88ab306004c08782103dca469bc02e897
Unknown <pilotpatrick2000@gmail.com>
2018-01-13 23:47:07 -0800

    Added jQuery
    Created jQuery and fixed the data upload function. Also split JS into two files for easier sorting of upload and download functions.

--- dataUpload.js
+++ dataUpload.js
@@ -1,234 +1,100 @@
-BlueAllianceURL = "https://www.thebluealliance.com/api/v2/event/";
-xbtAPP_ID = "?X-TBA-App-Id=FRC1983:Scouting:v2";
-DatabaseURL = "http://ec2-52-42-95-150.us-west-2.compute.amazonaws.com:8080";
-// Global Variables //
 var matches;
+var path = "http://ubuntu@ec2-52-35-34-216.us-west-2.compute.amazonaws.com:3000"; //TODO: Put in AWS info
 
+window.addEventListener('load', function() {
+    table = document.getElementById("matchData");
 
-window.onload = function() {
-  table = document.getElementById("matchData");
-
-  matches = [];
-  events = ["2016wagg","2018waahs","2018wasno","2018idbo","2018pncmp"];
-  for (var j=0; j<events.length; j++) {
-    eventName = events[j];
-    for (var i=0; i<localStorage.length; i++)	{
-  		var key = localStorage.key(i);
-      //console.log(key);															// Test
-      	if (eventName.concat("Matches") == key)
-      	{
-      		isThere = true;
-          checkForMatches(true);
-      		break;
-      	}
-  	}
-  }
-  for (var key in localStorage) {
-    try {
-      key = JSON.parse(localStorage.getItem(key));
-      isTele = key.isTele;
-      if(!key.isTele) {
-        console.log("Not a Match");
-        continue;
-      }
-      matches.push(key);
-    } catch(err) {
-      console.log("Not a Match");
-      continue;
+    matches = [];
+    for (var key in localStorage) {
+        try {
+            key = JSON.parse(localStorage.getItem(key));
+            isTele = key.isTele;
+            if(!key.isTele) {
+                console.log("Not a Match");
+                continue;
+            }
+            console.log("Pushing match");
+            matches.push(key);
+        } catch(err) {
+            console.log("Not a Match");
+            continue;
+        }
     }
-  }
 
-  matches.sort(function(a, b) {
-    return parseInt(a.match) - parseInt(b.match);
-  });
-  matches.forEach(function(thing) {
-    console.log("Good Match");
+    console.log(matches);
+
+    matches.sort(function(a, b) {
+        return parseInt(a.match) - parseInt(b.match);
+    });
+
+    matches.forEach(function(oneMatch) {
+        console.log("Good Match");
+        row = table.insertRow(-1);
+        name = oneMatch.match;
+        row.id = "row-" + name.toString();
+
+        match = row.insertCell(-1);
+        team = row.insertCell(-1);
+        sent = row.insertCell(-1);
+        submit = row.insertCell(-1);
+
+        match.innerHTML = oneMatch.match;
+        team.innerHTML = oneMatch.teamNo;
+        sent.innerHTML = "No";
+        submit.innerHTML = "<button id=\"" + name + "\" onclick=\"sendData(" + name + ");\">Submit Data</button>";
+    });
+});
+
+function sendData(match) {
+    console.log("Data sending for " + match + "...");
+
+    matches.forEach(function(oneMatch) {
+        if(oneMatch.match == match) {
+            post(oneMatch);
+        }
+    });
+
+    table = document.getElementById("send_messages");
     row = table.insertRow(-1);
-    name = thing.match;
-    row.id = "row-" + name.toString();
+    toDelete = document.getElementById("row-" + match.toString());
 
-    match = row.insertCell(-1);
-    team = row.insertCell(-1);
-    sent = row.insertCell(-1);
-    submit = row.insertCell(-1);
-
-    match.innerHTML = thing.match;
-    team.innerHTML = thing.teamNo
-    sent.innerHTML = "No";
-    submit.innerHTML = "<button id=\"" + name + "\" onclick=\"sendData(" + name + ");\">Submit Data</button>";
-  });
+    //TODO: Don't assume that the server actually got it.
+    row.innerHTML = "<p>Data sent for match " + name.toString() + ".</p>";
+    toDelete.parentNode.removeChild(toDelete);
+    /*for (var key in localStorage) {
+        try {
+            content = JSON.parse(localStorage.getItem(key));
+            thisMatch = content.match
+            if(!content.isTele) {
+                continue;
+            }
+            if(thisMatch == match) {
+                localStorage.removeItem(key);
+            }
+        } catch(err) {
+            continue;
+        }
+    }*/
 };
 
-function sendData(name) {
-  table = document.getElementById("send_messages");
-  row = table.insertRow(-1);
+function post(parameters) {
+    var form = $('<form id="upload"></form>');
 
-  row.innerHTML = "<p>Data sent for match " + name.toString() + ".</p>";
-  row.parentNode.removeChild(row);
-};
+    form.attr("method", "post");
+    form.attr("action", path);
 
-function download() {
-  eventName = document.getElementById("eventSelect").value;
-	isThere = false;
-	for (i=0; i<localStorage.length; i++)
-	{
-		var key = localStorage.key(i);
-    //console.log(key);															// Test
-    	if (eventName.concat("Matches") == key)
-    	{
-    		isThere = true;
-        checkForMatches(true);
-    		break;
-    	}
-	}
-	if (!isThere)
-	{
-		chrs = BlueAllianceURL.concat(eventName);
-		chrs = chrs.concat("/matches");
-    //console.log(chrs.concat(xbtAPP_ID));							// Test
-		issueRequestHTTP("GET", chrs.concat(xbtAPP_ID), function(result)
-		{
-			matchesHaveBeenDownloaded(result);
-		});
-	}
-	console.log("data downloaded");
-}
+    $.each(parameters, function(key, value) {
+        var field = $('<input></input>');
 
-function matchesHaveBeenDownloaded(contents) {
-  console.log("Hi");
-  var alliances;
-	var i, j;
-	var red, blue;
-	var sched;																// Our schedule (in JSON)
-	matches = new Array();											// Match schedule
-	var parsed = JSON.parse(contents);
-	j = 0;
-	for (i=0; i<parsed.length; i++)
-	{
-		if (parsed[i].comp_level == "qm")
-		{
-			red = parsed[i].alliances.red;
-			blue = parsed[i].alliances.blue;
-			matches[j] = [Number(parsed[i].match_number), Number(red.teams[0].substring(3,red.teams[0].length)), Number(red.teams[1].substring(3,red.teams[1].length)), Number(red.teams[2].substring(3,red.teams[2].length)),Number(blue.teams[0].substring(3,blue.teams[0].length)),  Number(blue.teams[1].substring(3,blue.teams[1].length)),  Number(blue.teams[2].substring(3,blue.teams[2].length))];
-			j++;
-		}
-	}
-	sortMatches(matches);
-	generateJSON(matches);
-}
+        field.attr("type", "hidden");
+        field.attr("name", key);
+        field.attr("value", value);
 
-function sortMatches(sched) {
-  console.log("hi");
-  var i, j, k; var swapped;
-	var temp;
-	swapped = true;
-	j = 0;
-	while (swapped)
-	{
-		swapped = false;
-		j++;
-		for (i = 0; i < sched.length - j; i++)
-		{
-			k = i + 1;
-			if (sched[k][0] < sched[i][0])
-			{
-				swapped = true;
-				temp = [sched[k][0],sched[k][1],sched[k][2],sched[k][3],sched[k][4],sched[k][5],sched[k][6]];
-				sched[k] = sched[i];
-				sched[i] = temp;
-			}
-		}
-	}
-}
+        form.append(field);
+    });
 
-function generateJSON(matrix) {
-  console.log("Hi");
-  var i;
-  var row = newMatch(matrix[0][0], matrix[0][1], matrix[0][2], matrix[0][3], matrix[0][4], matrix[0][5], matrix[0][6]);
-  //console.log(row);													// Test
-  var parsed = new Array(row);
-  for (i=1; i<matrix.length; i++)
-  {
-    parsed[i] = newMatch(matrix[i][0], matrix[i][1], matrix[i][2], matrix[i][3], matrix[i][4], matrix[i][5], matrix[i][6]);
-  }
-  var lsName = eventName.concat("Matches");
-  localStorage.setItem(lsName, JSON.stringify(parsed));
-}
-function issueRequestHTTP(reqType, URL, callback) {
-  console.log("Hi");
-	var xhr = new XMLHttpRequest();
-	xhr.onload = function (e)
-	{
-		if (xhr.readyState == 4)
-		{
-			if (xhr.status == 200)
-			{
-				if (xhr.responseText == "Not found")
-				{
-					issueError(' Not found.', false);
-				} else {
-					callback(xhr.responseText);
-				}
-			} else {
-				issueError(' HTTP returns ' + xhr.status, false);
-			}
-		} else issueError(String(' ').concat(xhr.readyState), false);										// Test
-	};
-	xhr.open(reqType, URL, true);
-	xhr.send(null);
-	xhr.onerror = function (e)
-	{
-		issueError(xhr.statusText, false);
-		console.log(xhr.statusText);																						// Test
-	}
-  console.log("hi");
-}
-function issueHTTPForm(formObj, URL, callback) {
-  console.log("Hi");
-	var xhr = new XMLHttpRequest();
-	xhr.onload = function (e)
-	{
-		if (xhr.readyState == 4)
-		{
-			if (xhr.status == 200)
-			{
-				if (xhr.responseText == "Not found")
-				{
-					issueError(' Not found.', false);
-				} else {
-					callback(xhr.responseText);
-				}
-			} else {
-				issueError(' HTTP returns ' + xhr.status, false);
-			}
-		} else issueError(String(' ').concat(xhr.readyState), false);										// Test
-	};
-	xhr.open("POST", URL, true);
-	var fData = new FormData();
-	fData.set("query",formObj);
-	xhr.send(fData);
-	xhr.onerror = function (e)
-	{
-		issueError(xhr.statusText, false);
-		console.log(xhr.statusText);																						// Test
-	}
-}
-function newMatch(matchNo, red1, red2, red3, blue1, blue2, blue3) {
-  console.log("Hi");
-  checkForMatches(true);
-//	console.log('{"matchNo":' + matchNo + ',"red":[', red1 + ',' + red2 + ',' + red3 + '], "blue":[' + blue1 + ',' + blue2 + ',' + blue3 + ']}');
-  return JSON.parse('{"matchNo":' + matchNo + ',"red":[' + red1 + ',' + red2 + ',' + red3 + '],"blue":[' + blue1 + ',' + blue2 + ',' + blue3 + ']}');
-}
-
-function issueError(msg, writeOver) {
-	console.log(msg);
-}
-
-function checkForMatches(statement){
-  elem = document.getElementById("matchesImage");
-  if (statement === true) {
-    elem.src = "images/greenCheck.png";
-  } else {
-    elem.src = "images/redX.png";
-  }
+    // The form needs to be a part of the document in
+    // order for us to be able to submit it.
+    $(document.body).append(form);
+    $.post(path, $('#upload').serialize())
 }
