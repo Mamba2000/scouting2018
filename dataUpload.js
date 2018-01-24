f7911a74dc7254393d74319d227d8f5f96507355
Perfectson Le <potatosaur69@gmail.com>
2018-01-23 20:21:29 -0800

    Revert "Merge branch 'dev' into mainpage"
    This reverts commit 73054f333a2fe73c1f12f7660d15a2acf1d61582.

--- dataUpload.js
+++ dataUpload.js
@@ -1,11 +1,5 @@
 var matches;
-const DEBUG = false;
-
-if(DEBUG) {
-    var path = "http://localhost:3000";
-} else {
-    var path = "http://ubuntu@ec2-52-35-34-216.us-west-2.compute.amazonaws.com:3000"; //TODO: Put in AWS info
-}
+var path = "http://ubuntu@ec2-52-35-34-216.us-west-2.compute.amazonaws.com:3000"; //TODO: Put in AWS info
 
 window.addEventListener('load', function() {
     table = document.getElementById("matchData");
@@ -52,16 +46,19 @@ window.addEventListener('load', function() {
 function sendData(match) {
     console.log("Data sending for match" + match + "...");
 
-    toDelete = document.getElementById("row-" + match.toString());
-
     matches.forEach(function(oneMatch) {
         if(oneMatch.match == match) {
-            console.log("Ping pong");
-            post(oneMatch, match, toDelete);
+            post(oneMatch);
         }
     });
 
-    //TODO: put this code inside the success condition for post()
+    table = document.getElementById("send_messages");
+    row = table.insertRow(-1);
+    toDelete = document.getElementById("row-" + match.toString());
+
+    //TODO: Don't assume that the server actually got it.
+    row.innerHTML = "<p>Data sent for match " + match + ".</p>";
+    toDelete.parentNode.removeChild(toDelete);
     /*for (var key in localStorage) {
         try {
             content = JSON.parse(localStorage.getItem(key));
@@ -78,7 +75,7 @@ function sendData(match) {
     }*/
 };
 
-function post(parameters, match, toDelete) {
+function post(parameters) {
     var form = $('<form id="upload"></form>');
 
     form.attr("method", "post");
@@ -97,19 +94,5 @@ function post(parameters, match, toDelete) {
     // The form needs to be a part of the document in
     // order for us to be able to submit it.
     $(document.body).append(form);
-
-    table = document.getElementById("send_messages");
-    row = table.insertRow(-1);
-
-    //TODO: Check if connection to server is a-ok
-    $.post(path, $('#upload').serialize(), function(data) {
-        if(data !== "success") {
-            row.innerHTML = "<p>Upload failed: " + data + "</p>";
-        } else {
-            row.innerHTML = "<p>Data sent for match " + match + ".</p>";
-            toDelete.parentNode.removeChild(toDelete);
-        }
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-        row.innerHTML = "<p>Upload failed: Server not found</p>";
-    });
+    $.post(path, $('#upload').serialize())
 }
