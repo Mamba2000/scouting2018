5ce1d9122ae1826ab581492000cff92fc5603981
Frishholz <evan.frishholz@gmail.com>
2018-01-25 20:12:52 -0800

    fixing splashAutoTele

--- splashAutoTele.js
+++ splashAutoTele.js
@@ -1,23 +1,24 @@
 //Global constants
 RED = "#BE1E2D";
 BLUE = "#1E2BBE";
-
+BlueAllianceURL = "https://www.thebluealliance.com/api/v2/event/";
+xbtAPP_ID = "?X-TBA-App-Id=FRC1983:Scouting:v2";
 //Global variables
-var robot = 0;
+var robot;
+var alliance;
+var jStr;
+var jObj;
+var LSName;											    	// List of teams
+var matches;
+var eventName;
+var teamNo;
 
 //initialize the splash sheet
 function initialize() {
+	jStr = '{"isTele":true,"scoutName":"default","eventName":"default","teamNo":0,"match":0,"alliance":"OOOO","autoStartPos":"default","autoCrossLine":false,"autoScale":0,"autoSwitch":0,"noShow":false,"teleScale":0,"teleSwitch":0,"teleExchange":0,"deadBot":false,"Climb":false,"AssistedClimb":0,"ReceivedClimb":false,"Park":false}';
+    jObj = JSON.parse(jStr);
 	var str = window.location.search;
- 	var tabletID = str.split("=");
-	matchTablet(tabletID[1]);
-	scout = localStorage.getItem("ScoutName");
-	if (scout != null) {
-		document.getElementById('scoutselect').value = scout;
-	}
-	evnt = localStorage.getItem("Event");
-	if (evnt != null) {
-		document.getElementById('eventselect').value = evnt;
-	}
+ 	matchTablet(str);
 }
 
 //Determine which tablet is doing the scouting from splashPage input
@@ -49,97 +50,37 @@ function matchTablet(argument){
 			break;
 		default:
 			alliance = "ANY";
-			robot = 0;
+			robot = 1;
 			break;
-	}
-	document.getElementById("splashSheetHeader").innerHTML = alliance + " " + robot;
-	if (alliance === "BLUE") {
-		document.getElementById("splashSheetHeader").style.color = "blue";
-	}
-	else if (alliance === "RED") {
-		document.getElementById("splashSheetHeader").style.color = "red";
 	}
 }
 
 //initialize the auto and tele part of the app
 function autoInitialize(){
-	alliance = localStorage.getItem("splash_alliance");
-	eventName = localStorage.getItem("splash_eventName");
-
-	if ((eventName == "test") || (eventName == null)) {
-		eventName = "2016wagg";		// Test
-	}
+	eventName = "2016wagg";
 	for (i=0; i<localStorage.length; i++) {
-		var key = localStorage.key(i);
-		//console.log(key);															// Test
-		if (eventName.concat("Matches") == key) {
-			isThere = true;
+			var key = localStorage.key(i);
+			//console.log(key);															// Test
+  		if (eventName.concat("Matches") == key) {
+  			isThere = true;
 		}
 	}
-
 	if (isThere) {
 		var jList = localStorage.getItem(eventName.concat("Matches"));
 		//console.log(jList);
 		matches = new Array();
-		var matches = JSON.parse(jList);
-	//		console.log("matches: " + matches.length);									// Test
-		var match = Number(localStorage.getItem("splash_match"));
-		if (match != null) {
-			elem = document.getElementById("teamNo");
-			title = document.getElementById("autoHeader");
-			sub = document.getElementById("autoSubmit");
-	//			console.log("Match: ".concat(match));							// Test
-
-			if (alliance === "RED") {
-				teamNo = matches[match - 1].red[robot - 1];
-				/*elem.innerHTML = teamNo;
-				elem.style.color = RED;
-				title.style.color = RED;
-				sub.style.color = RED;
-				sub.style.borderColor = RED;*/
-			} else {
-				teamNo = matches[match - 1].blue[robot - 1];
-				/*elem.innerHTML = teamNo;
-				elem.style.color = BLUE;
-				title.style.color = BLUE;
-				sub.style.color = BLUE;
-				sub.style.borderColor = BLUE;*/
-			}
-
-			elem = document.getElementById("teamNoTele");
-			title = document.getElementById("teleHeader");
-			sub = document.getElementById("teleSubmit");
-
-			if (alliance === "RED") {
-				teamNo = matches[match - 1].red[robot - 1];
-				/*elem.innerHTML = teamNo;
-				elem.style.color = RED;
-				title.style.color = RED;
-				sub.style.color = RED;
-				sub.style.borderColor = RED;*/
-			} else {
-				teamNo = matches[match - 1].blue[robot - 1];
-				/*elem.innerHTML = teamNo;
-				elem.style.color = BLUE;
-				title.style.color = BLUE;
-				sub.style.color = BLUE;
-				sub.style.borderColor = BLUE;*/
-			}
+		matches = JSON.parse(jList);
+//		console.log("matches: " + matches.length);									// Test
+		var match = parseInt(document.getElementById("matchNumber").value);
+		if (alliance === "RED") {
+			teamNo = matches[match - 1].red[robot - 1];
+		} else {
+			teamNo = matches[match - 1].blue[robot - 1];
 		}
+		document.getElementById("autoTeamNum").innerHTML = teamNo;
+		document.getElementById("teleTeamNum").innerHTML = teamNo;
 	}
-}
-function validateInp(elem) {
-	var validChars = /[0-9]/;
-	var strIn = elem.value;
-	var strOut = '';
-    for (var i=0; i < strIn.length; i++) strOut += (validChars.test(strIn.charAt(i)))? strIn.charAt(i) : '';
-    elem.value = strOut;
-}
-
-function fakeRadioButtons(set, choice){
-	var optionSet = $(set);
-	console.log(optionSet);
-
+	console.log(teamNo);
 }
 
 function changeCounter(field, condition){
@@ -151,21 +92,58 @@ function changeCounter(field, condition){
 	}
 }
 
-
-//need to clean this shit up with an array
 function switchPage(currentPage, direction){
-	if(currentPage == 'splash' && direction == 'forward'){
+	var pages = ['splashPage', 'autoPage', 'telePage'];
+	var currentPos = pages.indexOf(currentPage);
+	if(currentPos == 0){
 		autoInitialize();
-		document.getElementById('splashPage').hidden = true;
-		document.getElementById("autoPage").hidden = false;
-	} else if(currentPage == 'auto' && direction == 'forward'){
-		document.getElementById("autoPage").hidden = true;
-		document.getElementById("telePage").hidden = false;
-	} else if(currentPage == 'tele' && direction == 'backward'){
-		document.getElementById("autoPage").hidden = false;
-		document.getElementById("telePage").hidden = true;
-	} else if(currentPage == 'auto' && direction == 'backward'){
-		document.getElementById('splashPage').hidden = false;
-		document.getElementById("autoPage").hidden = true;
 	}
+	if(direction === 'forward'){
+		document.getElementById(pages[currentPos]).hidden = true;
+		document.getElementById(pages[currentPos+1]).hidden = false;
+	} else {
+		document.getElementById(pages[currentPos]).hidden = true;
+		document.getElementById(pages[currentPos-1]).hidden = false;
+	}
+}
+
+
+//takes all the data from the three pages and puts them into one jObj, then puts that in local storage
+function submitTele() {
+    jObj.scoutName = document.getElementById("scoutSelect").value;
+	jObj.eventName = document.getElementById("eventSelect").value;
+	jObj.teamNumber = teamNo;
+	jObj.match = parseInt(document.getElementById("matchNumber")).value;
+	jObj.alliance = alliance;
+	if(document.getElementById("outsideStart").checked)	{
+		jObj.autoStartPos = "outside";
+	} else {
+		jObj.autoStartPos = "center";
+	}
+	jObj.autoCrossLine = document.getElementById("crossedLine").checked;
+	jObj.autoScale = parseInt(document.getElementById("autoScale").value, 10);
+	jObj.autoSwitch = parseInt(document.getElementById("autoSwitch").value, 10);
+	jObj.noShow = document.getElementById("noShow").checked;
+	jObj.teleScale = parseInt(document.getElementById("teleScale").value, 10);
+	jObj.teleSwitch = parseInt(document.getElementById("teleSwitch").value, 10);
+	jObj.teleExchange = parseInt(document.getElementById("teleExchange").value, 10);
+	if (document.getElementById("autoDeadBot").checked || document.getElementById("teleDeadBot").checked){
+		jObj.deadBot = true;
+	} else {
+		jObj.deadBot = false;
+	}
+	jObj.Climb = document.getElementById("climb").checked;
+	jObj.AssistedClimb = parseInt(document.getElementById("AssistOthersClimb").value, 10);
+	jObj.ReceivedClimb = document.getElementById("helpedClimb").checked;
+
+	LSName = jObj.eventName.concat("_");
+	LSName = LSName.concat(jObj.teamNumber);
+	LSName = LSName.concat("_");
+	LSName = LSName.concat(jObj.match);
+	LSName = LSName.concat("_");
+	LSName = LSName.concat("Object");
+
+	localStorage.setItem(LSName, JSON.stringify(jObj));
+
+	console.log(JSON.stringify(jObj));
 }
